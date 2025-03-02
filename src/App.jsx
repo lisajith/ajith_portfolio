7ea1a8113c9ef7ef4410a77cae9f6a4ff84843fa
lisajith <lisajith27@gmail.com>
2025-03-02 18:13:37 +0530

    gsap1

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
       gsap.to(element, {
         scrollTrigger: {
           trigger: element,
-          start: '-75 bottom',
+          start: '50 bottom',
           end: 'bottom 80%',
           scrub: true
         },
