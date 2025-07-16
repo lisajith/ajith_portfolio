a1be6a7232f3d195f13caa09e84417afba366efd
lisajith <108571827+lisajith@users.noreply.github.com>
2025-07-16 15:45:05 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,8 +23,9 @@ const About = () => {
         <div className='container'>
             <div className='bg-zinc-800/50 p-7 md:p-12 rounded-2xl reveal-up'>
                 <p className='text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]'>
-                    Welcome! This is Ajith Malle, a software web developer
-                    with a knack for crafting visually stunning highly functional websites.
+                    Hi! I’m <strong>Ajith Malle</strong>, a passionate <strong>Web Developer</strong> 
+                    skilled in <strong>React.js</strong>, JavaScript, and responsive design. I love building websites 
+                    that look stunning and perform smoothly. 
                     Combining creativity and technical expertise that excels in both appearance and performance
                 </p>
                 <div className='flex flex-wrap items-center gap-4 md:gap-7'>
