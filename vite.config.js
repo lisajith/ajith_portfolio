90989bd43a691caa346d584c912cffee3c81c6e6
Lisa <lisajith27@gmail.com>
2026-08-22 17:27:12 +0530

    Add PWA configuration for portfolio

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tailwindscrollbar from 'tailwind-scrollbar'
-
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +12,29 @@ export default defineConfig({
       ...tailwindcss(), // Use Tailwind CSS properly
       apply: 'build', // Apply the plugin in the build process
     },
-    react()
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Ajith Malle Portfolio',
+        short_name: 'Ajith Portfolio',
+        description: 'Personal Portfolio App of Ajith Malle',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/images/logo2.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/images/logo2.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })
